bd20316d2c2deec9379bc59e5eba71d65313bb41
varun <varun.1rn17ei042@gmail.com>
2021-09-28 14:08:39 +0530

    my first python programme

---
+++ hello.py
@@ -0,0 +1,16 @@
+no1 = 10
+no2 = 20
+no3 = 30
+if (no1 > no2 > no3):
+    print("biggest no1")
+elif (no2 > no3 > no1):
+    print("biggest no2")
+else:
+    print("biggest no3")
+
+name = str(input("enter name"))
+age = int(input("enter age"))
+sub = 100 - age
+year = 2021 + sub
+print("you will turn 100 years in")
+print(year)
